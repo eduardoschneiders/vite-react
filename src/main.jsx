525f146539b4ffba69dbf9f68ff0d13e044ca7ff
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2024-12-22 23:41:01 -0300

    Working first

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import React from 'react';
+
+import '@shopify/polaris/build/esm/styles.css'; // Importa os estilos do Polaris
+import './custom.css';
+import { AppProvider } from '@shopify/polaris';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <AppProvider>
+      asdf asdf
+      <App />
+    </AppProvider>
+  </StrictMode>
 )
