4e3c9390bac34408a9a8bc68a80c3a37cc12a22a
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2024-12-28 17:52:25 -0300

    Working

--- src/NewComponent.jsx
+++ src/NewComponent.jsx
@@ -1,99 +1,35 @@
-import {
-  IndexTable,
-  LegacyCard,
-  useIndexResourceState,
-  Text,
-  Badge,
-} from '@shopify/polaris';
+import { Page, Card, Text, BlockStack } from '@shopify/polaris';
+import { ExportIcon, PlusIcon } from '@shopify/polaris-icons';
 import React from 'react';
 
-export default function NewComponent() {
-  const orders = [
-    {
-      id: '1020',
-      order: '#1020',
-      date: 'Jul 20 at 4:34pm',
-      customer: 'Jaydon Stanton',
-      total: '$969.44',
-      paymentStatus: <Badge progress="complete">Paid</Badge>,
-      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
-    },
-    {
-      id: '1019',
-      order: '#1019',
-      date: 'Jul 20 at 3:46pm',
-      customer: 'Ruben Westerfelt',
-      total: '$701.19',
-      paymentStatus: <Badge progress="partiallyComplete">Partially paid</Badge>,
-      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
-    },
-    {
-      id: '1018',
-      order: '#1018',
-      date: 'Jul 20 at 3.44pm',
-      customer: 'Leo Carder',
-      total: '$798.24',
-      paymentStatus: <Badge progress="complete">Paid</Badge>,
-      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
-    },
-  ];
-  const resourceName = {
-    singular: 'order',
-    plural: 'orders',
-  };
-
-  const { selectedResources, allResourcesSelected, handleSelectionChange } =
-    useIndexResourceState(orders);
-
-  const rowMarkup = orders.map(
-    (
-      { id, order, date, customer, total, paymentStatus, fulfillmentStatus },
-      index,
-    ) => (
-      <IndexTable.Row
-        id={id}
-        key={id}
-        selected={selectedResources.includes(id)}
-        position={index}
-      >
-        <IndexTable.Cell>
-          <Text variant="bodyMd" fontWeight="bold" as="span">
-            {order}
-          </Text>
-        </IndexTable.Cell>
-        <IndexTable.Cell>{date}</IndexTable.Cell>
-        <IndexTable.Cell>{customer}</IndexTable.Cell>
-        <IndexTable.Cell>
-          <Text as="span" alignment="end" numeric>
-            {total}
-          </Text>
-        </IndexTable.Cell>
-        <IndexTable.Cell>{paymentStatus}</IndexTable.Cell>
-        <IndexTable.Cell>{fulfillmentStatus}</IndexTable.Cell>
-      </IndexTable.Row>
-    ),
-  );
-
+export default function NewComponent({ foo, products }) {
+  console.log(foo);
+  console.log(products);
   return (
-    <LegacyCard>
-      <IndexTable
-        resourceName={resourceName}
-        itemCount={orders.length}
-        selectedItemsCount={
-          allResourcesSelected ? 'All' : selectedResources.length
-        }
-        onSelectionChange={handleSelectionChange}
-        headings={[
-          { title: 'Order' },
-          { title: 'Date' },
-          { title: 'Customer' },
-          { title: 'Total', alignment: 'end' },
-          { title: 'Payment status' },
-          { title: 'Fulfillment status' },
-        ]}
-      >
-        {rowMarkup}
-      </IndexTable>
-    </LegacyCard>
+    <Page
+      fullWidth
+      title="Orders"
+
+      pagination={{
+        hasNext: true,
+      }}
+    >
+      <Card>
+        <BlockStack gap="200">
+          <Text as="h2" variant="headingSm">
+            Credit card {foo}
+            {/* {products} */}
+            {products.map((product, index) => (
+              <Text as="p" variant="bodyMd" key={index}>
+                {product.name}
+              </Text>
+            ))}
+          </Text>
+          <Text as="p" variant="bodyMd">
+            Credit card information saf sadf sdf asdf sadf asd fasdf asf asdf as asgsd asdf asd sdf asd fsa fasf asfasd sa asdf asdf asdf sfdsdf sdf fgs dfgsdfg sdfg sdfg sdfg sdfg sdfg sdfg sdfg sdfg
+          </Text>
+      </BlockStack>
+      </Card>
+    </Page>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,15 @@ import './custom.css';
 import { AppProvider } from '@shopify/polaris';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NewComponent from './NewComponent'; // Importe seu novo componente
+let products = [
+  { name: 'T-shirt', price: 20 },
+  { name: 'Jeans', price: 40 },
+  { name: 'Jacket', price: 60 },
+  { name: 'Sneakers', price: 50 },
+  { name: 'Hat', price: 15 },
+  { name: 'Socks', price: 5 },
+  { name: 'Scarf', price: 10 },
+]
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,7 +26,7 @@ createRoot(document.getElementById('root')).render(
       <Router>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/new" element={<NewComponent />} />
+          <Route path="/new" element={<NewComponent foo="FOOOO" products={products} />} />
         </Routes>
       </Router>
     </AppProvider>
